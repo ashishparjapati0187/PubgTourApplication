60159c4ba26eb1e1e506c59464cb83ae60d1f909
ashish <Ashish.Parjapati@cognizant.com>
2021-04-05 11:19:29 +0530

    credential removed

--- userservice/env-variable.sh
+++ userservice/env-variable.sh
@@ -1,4 +1,4 @@
 export MYSQL_DATABASE=pubg_tour
-export MYSQL_USER=root
-export MYSQL_PASSWORD=root
+export MYSQL_USER=
+export MYSQL_PASSWORD=
 export MYSQL_CI_URL=jdbc:mysql://localhost:3306/pubg_tour
